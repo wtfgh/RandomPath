f535d0bc5ed3f36d35a863054c8570f35d833af8
GblK1 <shuliatovkonstantin@gmail.com>
2025-10-19 20:18:33 +0300

    приоритетное направление/притяжение к нему ВСЕ

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import numpy as np
 import tkinter as tk
 import random
 
-import numpy.random
-
-
 def normalize_vector(v):
     return v / np.linalg.norm(v)
 
@@ -26,10 +23,11 @@ class RunningDot:
                                         self.position.x + size / 2, self.position.y + size / 2, fill = color)
         self.step = step
         self.direction = Vector2D()
-        self.possible_directions_to_take = 10
-        self.gravity_toward_prioritized_direction = 0
-        self.ticks_per_second = 20
+        self.possible_directions_to_take = 100
+        self.ticks_per_second = 5
         self.color = color
+        self.gravity_slider = None
+        self.priority_direction_slider = None
 
     def tick(self):
         x_position_before_moving = self.position.x
@@ -41,23 +39,21 @@ class RunningDot:
 
         root.after(1000 // self.ticks_per_second, self.tick)
 
-    def move_randomly_and_update_pos(self, prioritized_direction = (1, 1)):
-        possible_directions = []
+    def move_randomly_and_update_pos(self):
+        step = 180 / (self.possible_directions_to_take / 2)
+
+        gravity_toward_prioritized_direction = self.gravity_slider.get() / 100
+
+        angles = []
         for i in range(self.possible_directions_to_take):
-            possible_direction = normalize_vector((random.randint(-100, 100), random.randint(-100, 100)))
+            angles.append(-self.priority_direction_slider.get() + step * i)
+            angles.append(-self.priority_direction_slider.get() - step * i)
 
-            possible_directions.append(possible_direction)
+        possible_directions = [(np.cos(np.radians(x)), np.sin(np.radians(x))) for x in angles]
 
-        new_direction = Vector2D(*possible_directions[0])
 
-        for i in range(len(possible_directions)):
-            for j in range(len(possible_directions)):
-                if (angle_between_two_vectors(possible_directions[i], prioritized_direction)
-                    < angle_between_two_vectors(possible_directions[j], prioritized_direction)):
-                        possible_directions[i], possible_directions[j] = possible_directions[j], possible_directions[i]
-
-        possible_directions_with_priority = possible_directions[:1 if self.gravity_toward_prioritized_direction == 1
-        else int((1 - self.gravity_toward_prioritized_direction) * self.possible_directions_to_take)]
+        possible_directions_with_priority = possible_directions[:1 if gravity_toward_prioritized_direction == 1
+        else int((1 - gravity_toward_prioritized_direction) * self.possible_directions_to_take)]
 
         self.direction = Vector2D(*possible_directions_with_priority[random.randint(0, len(possible_directions_with_priority) - 1)])
 
@@ -73,9 +69,18 @@ x_window_size = 500
 y_window_size = 500
 
 canvas = tk.Canvas(root, width = x_window_size, height = y_window_size, background = 'black')
-canvas.pack()
+canvas.grid(row = 0, column = 1, rowspan = 10)
 
 dot1 = RunningDot()
+
+priority_direction_slider = tk.Scale(root, from_ = 0, to = 360, orient = 'horizontal', length = 200)
+priority_direction_slider.grid(row = 0, column = 0)
+
+gravity_slider = tk.Scale(root, from_ = 0, to = 100, orient = 'horizontal', length = 200)
+gravity_slider.grid(row = 1, column = 0)
+
+dot1.gravity_slider = gravity_slider
+dot1.priority_direction_slider = priority_direction_slider
 dot1.tick()
 
 root.mainloop()
--- test.py
+++ test.py
@@ -1,31 +1,5 @@
-import line_profiler
 import numpy as np
-import random
 
-def normalize_vector(v):
-    return v / np.linalg.norm(v)
+x = 225
 
-def angle_between_two_vectors(v, u):
-    v_norm = normalize_vector(v)
-    u_norm = normalize_vector(u)
-    dot = np.clip(np.dot(v_norm, u_norm), -1, 1)
-    return np.degrees(np.arccos(dot))
-
-def move_randomly_and_update_pos(self, prioritized_direction=(1, 1)):
-    possible_directions = []
-    for i in range(self.possible_directions_to_take):
-        possible_direction = normalize_vector((random.randint(-100, 100), random.randint(-100, 100)))
-
-        possible_directions.append(possible_direction)
-
-    new_direction = Vector2D(*possible_directions[0])
-
-    for i in range(len(possible_directions)):
-        for j in range(len(possible_directions)):
-            if (angle_between_two_vectors(possible_directions[i], prioritized_direction)
-                    < angle_between_two_vectors(possible_directions[j], prioritized_direction)):
-                possible_directions[i], possible_directions[j] = possible_directions[j], possible_directions[i]
-
-profiler1 = line_profiler.LineProfiler()
-profiler1(avg_rating1)()
-profiler1.print_stats()
+print(np.cos(np.radians(x)), np.sin(np.radians(x)))
